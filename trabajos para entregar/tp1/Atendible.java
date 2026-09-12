3632044bd96983cfc177b5cede55a147148f0cf4
juanma ruiz <134015274+ryusdar@users.noreply.github.com>
2026-09-12 20:07:33 -0300

    Add files via upload

---
+++ trabajos para entregar/tp1/Atendible.java
@@ -0,0 +1,5 @@
+public interface Atendible {
+    void atender();
+}
+
+
---
+++ trabajos para entregar/tp1/TurnoInvalidoException.java
@@ -0,0 +1,5 @@
+public class TurnoInvalidoException extends Exception {
+    public TurnoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+}
